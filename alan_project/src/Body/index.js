c9fb0a6d47712a19a00edb6a5d170ae3ecf2fe6e
Anthony-Navarro <foto.arn@gmail.com>
2022-09-11 14:13:27 -0700

    Create Body COmponent

---
+++ alan_project/src/Body/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BodyStyle } from "./style";
+
+function Body() {
+  return <BodyStyle>I am the body</BodyStyle>;
+}
+
+export default Body;
---
+++ alan_project/src/Body/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const BodyStyle = styled.div`
+  display: flex;
+  padding: 5px;
+  background-color: lightgrey;
+`;
+
+export { BodyStyle };
